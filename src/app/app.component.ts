12868169987640a5dc667d0b8fe46d8b0838f0f2
Vrezh Fedora <vrezhraiden@gmail.com>
2025-01-29 18:53:48 +0100

    login page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { LoginRegisterComponent } from './pages/auth/login-register/login-register.component'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, LoginRegisterComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'TPS-Test-frontend';
+    title = 'TPS-Test-frontend'
 }
